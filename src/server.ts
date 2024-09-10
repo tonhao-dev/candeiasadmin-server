a38959fa97e05a8d718c036662b107fc4dcf6242
Luis Santiago <luis.santiago@btcambio.com.br>
2024-09-09 23:04:06 -0500

    add message when running server

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`🚀 Server is running on port ${process.env.PORT || 5000} 🚀`);
+});
