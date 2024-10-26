5347a7b82c16da6f74b8e11d55a1772e0bebbcd8
Luis Santiago <luis.santiago@btcambio.com.br>
2024-10-26 12:10:11 -0500

    create migration for belt table

---
+++ src/database/migrations/03_create_belt.ts
@@ -0,0 +1,23 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('belt', table => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid());
+    table.string('name').notNullable();
+    table.string('title').notNullable();
+    table.string('colorHexCode').notNullable(); // #000000 or #000000,#ffffff using comma to separate
+    table
+      .string('belt_type_code')
+      .nullable()
+      .references('code')
+      .inTable('belt_type')
+      .onDelete('CASCADE');
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('deleted_at').defaultTo(null);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('belt');
+}
