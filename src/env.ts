579e0e0fd101a26f4127cd6f12304cc1c66a5474
Luis Santiago <luis.santiago@mblabs.com.br>
2025-02-26 00:44:17 -0500

    fix(env): update DATA_API_DB_SERVICE_HOST validation to accept any string

--- src/env.ts
+++ src/env.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   DATA_API_DB_NAME: z.string(),
-  DATA_API_DB_SERVICE_HOST: z.string().ip(),
+  DATA_API_DB_SERVICE_HOST: z.string(),
   DATA_API_DB_SERVICE_PORT: z.coerce.number().int().min(1).max(65535),
   DATA_API_DB_USER: z.string().default('postgres'),
   DATA_API_DB_PASSWORD: z.string().default('postgres'),
