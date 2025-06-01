e331ad3d4d8bd2e9611aeb702a237d78c9f05b3f
Luis Santiago <luis.santiago@techbiz.com.br>
2025-05-31 22:59:25 -0300

    feat(guardian): make phone field not nullable in guardian table

--- src/database/migrations/00_create_guardian.ts
+++ src/database/migrations/00_create_guardian.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('guardian', table => {
     table.uuid('id').primary().defaultTo(knex.fn.uuid());
     table.string('name').notNullable();
-    table.string('phone').nullable();
+    table.string('phone').notNullable();
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at').defaultTo(null);
